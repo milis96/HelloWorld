59cb396fc668607c22c74bbb89c5badbf507b5c4
Stefan Milivojevic <milis96@bk.ru>
2020-01-21 23:27:26 +0300

    first commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Стефан  Миливоевич on 21.01.2020.
+//  Copyright © 2020 Stefan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
