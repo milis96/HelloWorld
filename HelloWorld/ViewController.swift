929d5d69ac45c5bd7d534b8314e3fb92eaf01b9d
Stefan Milivojevic <milis96@bk.ru>
2020-01-22 13:54:07 +0300

    add button and text

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        text.layer.isHidden = true
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        if text.layer.isHidden == true {
+            text.layer.isHidden = false
+            startButton.setTitle("Clear text", for: .normal)
+        } else {
+            text.layer.isHidden = true
+            startButton.setTitle("Show button", for: .normal)
+        }
+        
+    }
+    
 }
 
